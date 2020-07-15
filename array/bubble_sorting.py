ac08d8b0afd470955f4a8514ba7a19ee3b377111
tanishPatel <tanishpatel0502@gmail.com>
2020-07-15 12:15:42 +0530

    second greatest in an array

---
+++ array/bubble_sorting.py
@@ -0,0 +1,19 @@
+
+print("Bubble sorting\n")
+a=[]
+n= int(input("enter no of elements"))
+print("enter elements")
+
+for i in range(n):
+ data=int(input())
+ a.append(data)
+
+ 
+for i in range(0,n):
+    for j in range(0,n-1):
+        if a[j] > a[j+1]:
+         tmp=a[j]
+         a[j]=a[j+1]
+         a[j+1] = tmp
+    
+print(a)
