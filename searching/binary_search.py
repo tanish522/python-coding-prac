1fd879cec6889f776fb68159e1a64920476c3555
tanishPatel <tanishpatel0502@gmail.com>
2020-07-15 11:56:14 +0530

    bubble sort and binary search implemented

---
+++ searching/binary_search.py
@@ -0,0 +1,24 @@
+# binary search in python
+
+# Hello World program in Python
+    
+print "Hello World!\n"
+a=[1,5,10,20,40,50,100]
+key = input("enter key :")
+low=0
+high=6
+flag=0
+
+while low<=high:
+ mid=(low+high)/2
+ if key == a[mid]:
+  print("key is present at ",mid)
+  flag=1
+  break
+ elif key<a[mid]:
+  high=mid-1
+ else:
+  low=mid+1
+ 
+if flag == 0:
+ print("key not found")
